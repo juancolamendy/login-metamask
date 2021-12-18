dee76e01bd737959e101821230dd8b19eac9ac5f
Juan Carlos Olamendy <juanc.olamendy@gmail.com>
2021-12-18 09:24:39 -0500

    add metamask connect

--- pages/index.js
+++ pages/index.js
@@ -2,16 +2,29 @@ import React from 'react';
 import { useEthers } from "@usedapp/core";
 import { ethers } from 'ethers';
 import Web3Modal from "web3modal";
+import WalletConnectProvider from '@walletconnect/web3-provider';
 
 export default function Home() {
   // hooks
   const { library } = useEthers();
 
   // functions
-  const handleMetamaskConnect = async () => {
+  const handleConnect = async () => {
     try {
+      const providerOptions = {
+        walletconnect: {
+          package: WalletConnectProvider,
+          options: {
+            // test key - don't copy as your mileage may vary
+            infuraId: "8043bb2cf99347b1bfadfb233c5325c0",
+          }
+        },
+      }; 
       // web3Modal support multiple providers/wallets
-      const web3Modal = new Web3Modal();
+      const web3Modal = new Web3Modal({
+        cacheProvider: false, // optional
+        providerOptions, // required        
+      });
       const connection = await web3Modal.connect();
       // Get providers
       const provider = new ethers.providers.Web3Provider(connection);
@@ -34,7 +47,7 @@ export default function Home() {
         <button
           type="submit"
           className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-          onClick={handleMetamaskConnect}
+          onClick={handleConnect}
         >
           { library ? 'Connected ...' : 'Connect With MetaMask' }
         </button>
